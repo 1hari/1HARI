b1f7dfa7c822f1748b27fa2c949943f5688008fe
hyungnam <zzzsh789@naver.com>
2020-01-08 17:18:21 +0900

    시큐리티 페이지 권한 설정중

--- src/main/java/kr/coo/onehari/home/controller/HomeController.java
+++ src/main/java/kr/coo/onehari/home/controller/HomeController.java
@@ -1,6 +1,13 @@
 package kr.coo.onehari.home.controller;
 
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,5 +22,11 @@ public class HomeController {
 	public String main() {
 		return "1hari.main";
 	}
+	
+	//형남 0108 1640~
+    @RequestMapping(value="/error403.hari")
+    public String accessDeniedPage() throws Exception {
+        return "1hari.403";
+    }
 }
 
