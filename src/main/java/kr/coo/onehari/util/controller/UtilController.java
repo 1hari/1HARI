a26931475f3297e32ba537ee0cb90518065c5bae
thelunor <thelunor@gmail.com>
2020-01-13 15:42:59 +0900

    Excel 업로드 다운로드 진행중 커밋시간 준수

--- src/main/java/kr/coo/onehari/util/controller/UtilController.java
+++ src/main/java/kr/coo/onehari/util/controller/UtilController.java
@@ -1,8 +1,13 @@
 package kr.coo.onehari.util.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.coo.onehari.hr.dto.Excel;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -10,5 +15,11 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("util/")
 public class UtilController {
 
-	
+	@RequestMapping("excel.hari")
+	public String excelDownload(Model model) {
+		List<Excel> excelList = Arrays.asList(
+//				new Excel(11, 12, 13, 14, "첫번째 줄")
+				);
+		return null;
+	}
 }
