650cc770f4ec3eab1b01dc9ea690320b688c0f81
thelunor <thelunor@gmail.com>
2020-01-13 14:34:15 +0900

    Excel 업로드/다운로드 기본설정

---
+++ src/main/java/kr/coo/onehari/util/controller/UtilController.java
@@ -0,0 +1,14 @@
+package kr.coo.onehari.util.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("util/")
+public class UtilController {
+
+	
+}
