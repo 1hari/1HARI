52bc0996c732b23c4a9efcaf28314bacd8d5ac2d
chansik <yangchan-sik@nate.com>
2020-01-15 12:33:43 +0900

    부서별 게시판 crud

--- src/main/webapp/resources/vue/components/depart/departMain.vue
+++ src/main/webapp/resources/vue/components/depart/departMain.vue
@@ -8,59 +8,44 @@
         <table class="table table-hover" id="board_list" >
           <thead>
             <tr>
-              <th class="text-center d-none d-md-table-cell">번호</th>
-              <th class="text-center d-none d-md-table-cell">제목</th>
-               <th class="text-center d-none d-md-table-cell">첨부파일</th>
+              <th class="text-center d-none d-md-table-cell">글번호</th>
+              <th class="w-50">제목</th>
+              <th class="text-center d-none d-md-table-cell">글내용</th>
               <th class="text-center d-none d-md-table-cell">작성자</th>
               <th class="text-center d-none d-md-table-cell">작성일</th>
-          
             </tr>
           </thead>
 
           <tbody>
-            <tr>
-              <td class="text-center d-none d-md-table-cell">1</td>
-              <td class="text-center d-none d-md-table-cell">2</td>
-              <td class="text-center d-none d-md-table-cell">3</td>
-              <td class="text-center d-none d-md-table-cell">4</td>
-              <td class="text-center d-none d-md-table-cell">5</td>
+            <tr v-for="a1 in server_data.board_list">
+              <td class="text-center d-none d-md-table-cell">{{a1.boardNum}}</td>
+              <td class="text-center d-none d-md-table-cell">{{a1.boardTitle}}</td>
+              <td class="text-center d-none d-md-table-cell">{{a1.boardContent}}</td>
+              <td class="text-center d-none d-md-table-cell">{{a1.writer}}</td>
+              <td class="text-center d-none d-md-table-cell">{{a1.bWriteDate}}</td>
             </tr> 
-          </tbody>
-          <tbody>
-            <tr v-for="a1 in server_data.board_list" :key="a1" >
-              <td class="text-center d-none d-md-table-cell">{{a1.content_idx}}</td><!--기본급으로 받으세요 -->
-              <td @click="go_board_read(a1.content_idx)">{{a1.content_subject}}</td><!--국민연금으로 데이터 받으세요-->
-              <td class="text-center d-none d-md-table-cell">{{a1.content_writer_name}}</td>
-              <td class="text-center d-none d-md-table-cell">{{a1.content_writer_name}}</td>
-              <td class="text-center d-none d-md-table-cell">{{a1.content_writer_name}}</td>
-              <td class="text-center d-none d-md-table-cell">{{a1.content_writer_name}}</td>
-              <td class="text-center d-none d-md-table-cell">{{a1.content_writer_name}}</td>
-              <td class="text-center d-none d-md-table-cell">{{a1.content_writer_name}}</td>
-              <td class="text-center d-none d-md-table-cell">{{a1.content_date}}</td><!--실급여로 데이터 받으세요 -->
-              <td class="text-center d-none d-md-table-cell"><button class ="btn btn-sucess" style = "background-color: #2ab2aa; text-color:white;">급여명세</button></td>
-            </tr>
           </tbody>
         </table>
 
         <div class="d-none d-md-block">
           <ul class="pagination justify-content-center">
             <li class="page-item">
-              <router-link :to="'/board_main/' + $route.params.board_idx + '/' + server_data.pre" class="page-link">이전</router-link>
+              <router-link :to="'/board_main/' + $route.params.boardNum + '/' + server_data.pre" class="page-link">이전</router-link>
             </li>
             <li class="page-item" v-for="page in server_data.page_array" :key="page">
-              <router-link :to="'/board_main/' + $route.params.board_idx + '/' + page" class="page-link">{{page}}</router-link>
+              <router-link :to="'/board_main/' + $route.params.boardNum + '/' + page" class="page-link">{{page}}</router-link>
               <!--여러개의 파라미터를 전달할 때 :to 사용-->
             </li>
 
             <li class="page-item">
-              <router-link :to="'/board_main/' + $route.params.board_idx + '/' + server_data.next" class="page-link">다음</router-link>
+              <router-link :to="'/board_main/' + $route.params.boardNum + '/' + server_data.next" class="page-link">다음</router-link>
             </li>
           </ul>
         </div>
 
         <!--글쓰기--> 
         <div class="text-right"> <!--v-if='$store.state.user_login_chk == true'-->
-          <router-link :to="'/board_write/' + $route.params.board_idx" class="btn btn-primary">글쓰기</router-link>
+          <router-link :to="'/board_write/' + $route.params.boardNum" class="btn btn-primary">글쓰기</router-link>
         </div>
       </div>
     </div>
@@ -84,11 +69,11 @@ module.exports = {
     go_board_read: function(content_idx) {
       alert(content_idx)	
       //this.$router router 객체 불러오기
-      this.$router.push('/board_read/' + this.$route.params.board_idx + '/' + this.$route.params.page + '/' + content_idx) //.push에 파라미터값에 알맞는 컴포넌트를 찾아 그 컴포넌트 주소로 이동시켜줌
+      this.$router.push('/board_read/' + this.$route.params.boardNum + '/' + this.$route.params.page + '/' + boardNum) //.push에 파라미터값에 알맞는 컴포넌트를 찾아 그 컴포넌트 주소로 이동시켜줌
     },
     get_board_data: function() {
       var params = new URLSearchParams();
-      params.append("board_idx", this.$route.params.board_idx);
+      params.append("boardNum", this.$route.params.boardNum);
       params.append("page", this.$route.params.page)
       axios.post("get_board_list.do", params).then((response)=>{
       console.log(response.data)
@@ -97,7 +82,7 @@ module.exports = {
     }
   },
   created() {
-    //alert(this.$route.params.board_idx) //주소를 관리하는 객체 route, 주소가 바뀌면 route객체도 변경됨, route객체의 변경을 감지해줘야함
+    //alert(this.$route.params.boardNum) //주소를 관리하는 객체 route, 주소가 바뀌면 route객체도 변경됨, route객체의 변경을 감지해줘야함
     this.get_board_data();
   },
   watch: {
